c9753698ba2ec0fdf713b2fd62cccd292aedf391
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-07 08:43:46 -0300

    update spring boot (used older version of webjars for jquery)

--- ssc-brewery/src/main/java/guru/sfg/brewery/web/mappers/BeerOrderLineMapper.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/web/mappers/BeerOrderLineMapper.java
@@ -22,7 +22,7 @@ import guru.sfg.brewery.web.model.BeerOrderLineDto;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 
-@Mapper(uses = {DateMapper.class})
+@Mapper(componentModel = "spring", uses = {DateMapper.class})
 @DecoratedWith(BeerOrderLineMapperDecorator.class)
 public interface BeerOrderLineMapper {
     BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line);
--- ssc-brewery/src/test/java/guru/sfg/brewery/SfgBreweryUiApplicationTests.java
+++ ssc-brewery/src/test/java/guru/sfg/brewery/SfgBreweryUiApplicationTests.java
@@ -19,8 +19,11 @@ package guru.sfg.brewery;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SfgBreweryUiApplicationTests {
 
