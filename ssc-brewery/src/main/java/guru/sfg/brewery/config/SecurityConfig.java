2d2845cd5f3789a57fece1fec97d922a34ac96f8
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-09 08:33:47 -0300

    Spring Boot Test Context. Closes #35.

--- ssc-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,46 +1,21 @@
 package guru.sfg.brewery.config;
 
-import guru.sfg.brewery.security.RestHeaderAuthFilter;
-import guru.sfg.brewery.security.RestUrlAuthFilter;
 import guru.sfg.brewery.security.SFGPasswordEncoderFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager){
-        RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
-        filter.setAuthenticationManager(authenticationManager);
-        return filter;
-    }
-
-    public RestUrlAuthFilter restUrlAuthFilter(AuthenticationManager authenticationManager){
-        RestUrlAuthFilter filter = new RestUrlAuthFilter(new AntPathRequestMatcher("/api/**"));
-        filter.setAuthenticationManager(authenticationManager);
-        return filter;
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
-                UsernamePasswordAuthenticationFilter.class)
-                .csrf().disable();
-
-        http.addFilterBefore(restUrlAuthFilter(authenticationManager()),
-                UsernamePasswordAuthenticationFilter.class);
-
         http
                 .authorizeRequests(authorize -> {
                     authorize
@@ -63,20 +38,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     PasswordEncoder passwordEncoder(){
         return SFGPasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("spring")
-//                .password("{bcrypt}$2a$10$/K6J.1aazVKumTekHMF5b.b5Jsw4O4ja1vYURX.cV/vyXlUBbPRSW")
-//                .roles("ADMIN")
-//                .and()
-//                .withUser("user")
-//                .password("{sha256}ab781fb2e760eb3939cbde9419f1f451f73156e0d2cd808e1eb056cf4d348eac517ea72afdf5096b")
-//                .roles("USER");
-//
-//        auth.inMemoryAuthentication().withUser("scott")
-//                .password("{bcrypt10}$2a$10$prcnoMvNGgwHvW9xxhxWkemqHGVmZaV9tSplldIcQQ4NetUCaKrmy")
-//                .roles("CUSTOMER");
-//    }
 }
--- ssc-brewery/src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ ssc-brewery/src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -1,49 +1,16 @@
 package guru.sfg.brewery.web.controllers.api;
 
-import guru.sfg.brewery.security.AbstractRestAuthFilter;
 import guru.sfg.brewery.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@SpringBootTest
 public class BeerRestControllerIT extends BaseIT {
-
-    @Test
-    void deleteBeerUrl() throws Exception{
-        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
-                .header(AbstractRestAuthFilter.API_KEY, "spring")
-                .header(AbstractRestAuthFilter.API_SECRET, "guru"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void deleteBeerBadCredsUrl() throws Exception{
-        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
-                .header(AbstractRestAuthFilter.API_KEY, "spring")
-                .header(AbstractRestAuthFilter.API_SECRET, "guruXXXX"))
-                .andExpect(status().isUnauthorized());
-    }
-
-
-    @Test
-    void deleteBeerBadCreds() throws Exception{
-        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
-                .header(AbstractRestAuthFilter.API_KEY, "spring")
-                .header(AbstractRestAuthFilter.API_SECRET, "guruXXXX"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    void deleteBeer() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
-                .header(AbstractRestAuthFilter.API_KEY, "spring")
-                .header(AbstractRestAuthFilter.API_SECRET, "guru"))
-                .andExpect(status().isOk());
-    }
 
     @Test
     void deleteBeerHttpBasic() throws Exception{
