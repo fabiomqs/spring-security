01c504a3d4658c9da6d0579073a25148840e4526
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-09 08:08:04 -0300

    Spring Security Configuration. Closes #34.

--- ssc-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -64,19 +64,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return SFGPasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("spring")
-                .password("{bcrypt}$2a$10$/K6J.1aazVKumTekHMF5b.b5Jsw4O4ja1vYURX.cV/vyXlUBbPRSW")
-                .roles("ADMIN")
-                .and()
-                .withUser("user")
-                .password("{sha256}ab781fb2e760eb3939cbde9419f1f451f73156e0d2cd808e1eb056cf4d348eac517ea72afdf5096b")
-                .roles("USER");
-
-        auth.inMemoryAuthentication().withUser("scott")
-                .password("{bcrypt10}$2a$10$prcnoMvNGgwHvW9xxhxWkemqHGVmZaV9tSplldIcQQ4NetUCaKrmy")
-                .roles("CUSTOMER");
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("spring")
+//                .password("{bcrypt}$2a$10$/K6J.1aazVKumTekHMF5b.b5Jsw4O4ja1vYURX.cV/vyXlUBbPRSW")
+//                .roles("ADMIN")
+//                .and()
+//                .withUser("user")
+//                .password("{sha256}ab781fb2e760eb3939cbde9419f1f451f73156e0d2cd808e1eb056cf4d348eac517ea72afdf5096b")
+//                .roles("USER");
+//
+//        auth.inMemoryAuthentication().withUser("scott")
+//                .password("{bcrypt10}$2a$10$prcnoMvNGgwHvW9xxhxWkemqHGVmZaV9tSplldIcQQ4NetUCaKrmy")
+//                .roles("CUSTOMER");
+//    }
 }
