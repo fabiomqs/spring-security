23c3a243ac17678772f73d98f641466b319c6d6f
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-09 06:53:00 -0300

    Spring Data JPA Repositories. Closes #30.

---
+++ ssc-brewery/src/main/java/guru/sfg/brewery/repositories/security/AuthorityRepository.java
@@ -0,0 +1,6 @@
+package guru.sfg.brewery.repositories.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<AuthorityRepository, Integer> {
+}
