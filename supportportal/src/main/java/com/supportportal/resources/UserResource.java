a75de1486ffa8d45e690c9b64f5e2f5ed8b3f534
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-18 15:25:06 -0300

    Test custom exception handling. Closes #127.

--- supportportal/src/main/java/com/supportportal/resources/UserResource.java
+++ supportportal/src/main/java/com/supportportal/resources/UserResource.java
@@ -1,16 +1,22 @@
 package com.supportportal.resources;
 
 import com.supportportal.exception.ExceptionHandling;
+import com.supportportal.exception.domain.EmailExistException;
+import com.supportportal.exception.domain.UserNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
-public class UserResource extends ExceptionHandling {
-
+public class UserResource {//don't need to extend the controller advice
+    //
     @GetMapping("/home")
-    public String showUser() {
-        return "Application works!";
+    public String showUser() throws Exception {
+        //throw new EmailExistException("This email address is already taken!");
+        //throw new UserNotFoundException("This user was not found!");
+        throw new RuntimeException("Some other Runtime error!");
+        //return "Application works!";
+
     }
 }
