aa8ce93150ccffd2acfe944c3fed4973d06f13b3
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-18 14:59:44 -0300

    Custom exception handling. Closes #126.

--- supportportal/src/main/java/com/supportportal/resources/UserResource.java
+++ supportportal/src/main/java/com/supportportal/resources/UserResource.java
@@ -1,12 +1,13 @@
 package com.supportportal.resources;
 
+import com.supportportal.exception.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
-public class UserResource {
+public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
     public String showUser() {
