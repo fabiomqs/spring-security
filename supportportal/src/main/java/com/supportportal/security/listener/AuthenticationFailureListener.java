372c0f26d8f627ae73a623b6fa58b6f1e3c7a2d0
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-19 09:06:50 -0300

    Authentication failure listener. Closes #139.

---
+++ supportportal/src/main/java/com/supportportal/security/listener/AuthenticationFailureListener.java
@@ -0,0 +1,28 @@
+package com.supportportal.security.listener;
+
+import com.supportportal.service.LoginAttemptService;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class AuthenticationFailureListener {
+
+    private final LoginAttemptService loginAttemptService;
+
+    public AuthenticationFailureListener(LoginAttemptService loginAttemptService) {
+        this.loginAttemptService = loginAttemptService;
+    }
+
+    @EventListener
+    public void onAuthenticationFailure(
+            AuthenticationFailureBadCredentialsEvent event) throws ExecutionException {
+
+        if(event.getAuthentication().getPrincipal() instanceof String) {
+            String username = (String) event.getAuthentication().getPrincipal();
+            loginAttemptService.addUserToLoginAttemptCache(username);
+        }
+    }
+}
