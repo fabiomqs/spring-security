00b75f568c2285c6207c4df1a7456a2cc66a348e
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-20 09:45:15 -0300

    User service. Closes #149.

--- supportportal/src/main/java/com/supportportal/service/UserService.java
+++ supportportal/src/main/java/com/supportportal/service/UserService.java
@@ -2,16 +2,17 @@ package com.supportportal.service;
 
 import com.supportportal.domain.User;
 import com.supportportal.domain.UserPrincipal;
-import com.supportportal.exception.domain.EmailExistException;
-import com.supportportal.exception.domain.UserNotFoundException;
-import com.supportportal.exception.domain.UsernameExistException;
+import com.supportportal.exception.domain.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.List;
 
 public interface UserService {
 
-    User register(String firstName, String lastName, String username, String email) throws UserNotFoundException, EmailExistException, UsernameExistException, MessagingException;
+    User register(String firstName, String lastName, String username, String email)
+            throws UserNotFoundException, EmailExistException, UsernameExistException, MessagingException;
 
     List<User> getUsers();
 
@@ -20,4 +21,23 @@ public interface UserService {
     User findUserByEmail(String email);
 
     UserPrincipal login(User user);
+
+    User addNewUser(String firstName, String lastName, String username, String email, String role,
+                    boolean isNonLocked, boolean isActive, MultipartFile profileImage)
+            throws UserNotFoundException, UsernameExistException, EmailExistException, IOException,
+            NotAnImageFileException;
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername,
+                    String newEmail, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage)
+            throws UserNotFoundException, UsernameExistException, EmailExistException, IOException,
+            NotAnImageFileException;
+
+    void deleteUser(Long id);
+    //void deleteUser(String username) throws IOException;
+
+    void resetPassword(String email) throws MessagingException, EmailNotFoundException;
+
+    User updateProfileImage(String username, MultipartFile profileImage)
+            throws UserNotFoundException, UsernameExistException, EmailExistException, IOException,
+            NotAnImageFileException;
 }
