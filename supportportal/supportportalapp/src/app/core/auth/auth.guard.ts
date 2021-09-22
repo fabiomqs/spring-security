e0a0b312df315ac6ef74e5c42013a0bafb7d5ee5
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-22 11:30:44 -0300

    Notification types. Closes #199.

--- supportportal/supportportalapp/src/app/core/auth/auth.guard.ts
+++ supportportal/supportportalapp/src/app/core/auth/auth.guard.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { NotificationType } from 'src/app/enums/notification-type.enum';
 import { AuthenticattionService } from 'src/app/service/authenticattion.service';
+import { NotificationService } from 'src/app/service/notification.service';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
 
     constructor(
         private authenticattionService: AuthenticattionService,
-        private router: Router) {}
+        private router: Router,
+        private notificationService: NotificationService) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
 
@@ -19,7 +22,9 @@ export class AuthGuard implements CanActivate {
             return true;
         }
         this.router.navigate([`${this.authenticattionService.loginPath}`])
-        // TODO - Sendo Notification to User
+        this.notificationService
+                .notify(NotificationType.ERROR, 
+                        `You need to log in to access this page`.toUpperCase());
         return false;
     }
   
--- supportportal/supportportalapp/src/app/service/notification.service.ts
+++ supportportal/supportportalapp/src/app/service/notification.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { NotifierService } from 'angular-notifier';
+import { NotificationType } from '../enums/notification-type.enum';
 
 @Injectable({providedIn: 'root'})
 export class NotificationService {
 
     constructor(private notifier:NotifierService) { }
 
-    public notify(type: string, message: string): void {
+    public notify(type: NotificationType, message: string): void {
         this.notifier.notify(type, message);
     }
 
