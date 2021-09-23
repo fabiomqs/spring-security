2447a7615592aa7e8aa6fca7de9b6bd8de89cc45
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-23 08:06:35 -0300

    Configuring routes. Closes #202.

--- supportportal/supportportalapp/src/app/app-routing.module.ts
+++ supportportal/supportportalapp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './home/login/login.component';
+import { RegisterComponent } from './home/register/register.component';
+import { UserComponent } from './users/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'user/management', component: UserComponent},
+    {path: '', redirectTo: '/login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
