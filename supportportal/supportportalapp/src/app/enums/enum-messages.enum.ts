da87e54d536fd37c64209995f46f533573ad619f
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-24 06:48:20 -0300

    Register component. Closes #213.

--- supportportal/supportportalapp/src/app/enums/enum-messages.enum.ts
+++ supportportal/supportportalapp/src/app/enums/enum-messages.enum.ts
@@ -1,4 +1,6 @@
 export enum EnumMessages {
     UNKNOW_ERROR_MESSAGE = 'An Unknow Error Occurred, please try again!',
-    FORBIDEN_MESSAGE = 'You need to log in to access this page'
+    FORBIDEN_MESSAGE = 'You need to log in to access this page',
+    REGISTER_SUCCESS_PREFIX = 'A new account was created for ',
+    REGISTER_SUCCESS_SUFIX = 'Please check your e-mail for password to log in.'
 }
--- supportportal/supportportalapp/src/app/home/register/register.component.ts
+++ supportportal/supportportalapp/src/app/home/register/register.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { EnumMessages } from 'src/app/enums/enum-messages.enum';
+import { EnumRoutes } from 'src/app/enums/enum-routes.enum';
+import { NotificationType } from 'src/app/enums/notification-type.enum';
 import { User } from 'src/app/model/user';
+import { AuthenticationService } from 'src/app/service/authenticattion.service';
+import { NotificationService } from 'src/app/service/notification.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-register',
@@ -9,19 +17,49 @@ import { User } from 'src/app/model/user';
 export class RegisterComponent implements OnInit, OnDestroy {
 
     showLoading: boolean;
+    private subscriptions: Subscription[] = [];
 
-    constructor() { }
+    constructor(private router:Router,
+        private authenticationService: AuthenticationService,
+        private userService: UserService,
+        private notificationService: NotificationService) { }
     
+    ngOnInit(): void {
+        if(this.authenticationService.isUserLoggedIn()) {
+            this.router.navigateByUrl(`/${EnumRoutes.USERS}`);
+        }
+    }
 
     onRegister(user:User) {
+        this.showLoading = true;
+        console.log(user);
+        this.subscriptions.push(
+            this.userService.register(user).subscribe(
+                (response: User) => {
+                    this.showLoading = false;
+                    this.sendNotification(NotificationType.SUCCESS, 
+                    `${EnumMessages.REGISTER_SUCCESS_PREFIX}${response.username}.
+                        ${EnumMessages.REGISTER_SUCCESS_SUFIX}`);
+                    //this.router.navigateByUrl(`/${EnumRoutes.LOGIN}`);
+                },
+                error => {
+                    console.log(error);
+                    this.sendNotification(NotificationType.ERROR, error.error.message);
+                    this.showLoading = false;
+                }
+            )
+        );
     }
-    
 
-    ngOnInit(): void {
+    private sendNotification(type: NotificationType, message: string):void {
+        if(message) 
+            this.notificationService.notify(type, message);
+        else
+            this.notificationService.notify(NotificationType.ERROR, EnumMessages.UNKNOW_ERROR_MESSAGE);
     }
 
     ngOnDestroy(): void {
-        
+        this.subscriptions.forEach(sub => sub.unsubscribe());
     }
 
 }
