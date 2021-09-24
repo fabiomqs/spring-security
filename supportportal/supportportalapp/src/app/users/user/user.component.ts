cbc27b3c71c40e8715abbdb77489615fa135bead
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-24 08:29:30 -0300

    Adding tab name. Closes #216. Closes #217.

--- supportportal/supportportalapp/src/app/users/user/user.component.ts
+++ supportportal/supportportalapp/src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+    private titleSubject = new BehaviorSubject<string>('Users');
+    titleAction$ = this.titleSubject.asObservable();
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    changeTitle(title: string): void {
+        this.titleSubject.next(title);
+    }
+
+    ngOnInit(): void {
+    }
 
 }
