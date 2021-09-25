dc12e844c6c13ca94c90a8464c10dde9f2ee5017
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-25 17:41:23 -0300

    Add JWT Token Provider. Closes #265.

---
+++ alurapic/src/main/java/br/com/alura/alurapic/security/util/JwtTokenProvider.java
@@ -0,0 +1,99 @@
+package br.com.alura.alurapic.security.util;
+
+import br.com.alura.alurapic.domain.security.UserPrincipal;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static java.util.Arrays.stream;
+import static br.com.alura.alurapic.util.constant.SecurityConstant.*;
+
+@Slf4j
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateJwtToken(UserPrincipal userPrincipal) {
+        String[] claims = getClaimsFromUser(userPrincipal);
+        return JWT.create()
+                .withIssuer(ALURA)
+                .withAudience(ALURA_PIC)
+                .withIssuedAt(new Date())
+                .withSubject(userPrincipal.getUsername())
+                .withArrayClaim(AUTHORITIES, claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(HMAC512(secret.getBytes()));
+
+    }
+
+    public List<GrantedAuthority> getAuthorities(String token) {
+        //String[] claims = getClaimsFromToken(token);
+        return stream(getClaimsFromToken(token))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    public Authentication getAuthentication(String username,
+                                            List<GrantedAuthority> authorities,
+                                            HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(username, null, authorities);
+        usernamePasswordAuthenticationToken.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request));
+        return usernamePasswordAuthenticationToken;
+    }
+
+    public boolean isTokenValid(String username, String token) {
+        return StringUtils.isNoneEmpty(username) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        Date expirationDate = getJwtVerifier().verify(token).getExpiresAt();
+        return expirationDate.before(new Date());
+    }
+
+    public String getSubject(String token) {
+        return getJwtVerifier().verify(token).getSubject();
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        return getJwtVerifier()
+                .verify(token)
+                .getClaim(AUTHORITIES)
+                .asArray(String.class);
+    }
+
+    private JWTVerifier getJwtVerifier() {
+        JWTVerifier jwtVerifier;
+        try {
+            jwtVerifier = JWT.require(HMAC512(secret)).withIssuer(ALURA).build();
+        } catch (JWTVerificationException exception) {
+            log.error(TOKEN_CANNOT_BE_VERIFIED, exception);
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return jwtVerifier;
+    }
+
+    private String[] getClaimsFromUser(UserPrincipal user) {
+        return user.getAuthorities().stream().map(authority -> {
+            return authority.getAuthority();
+        }).collect(Collectors.toList()).toArray(new String[0]);
+    }
+}
--- alurapic/src/main/java/br/com/alura/alurapic/util/constant/SecurityConstant.java
+++ alurapic/src/main/java/br/com/alura/alurapic/util/constant/SecurityConstant.java
@@ -5,6 +5,11 @@ public class SecurityConstant {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String JWT_TOKEN_HEADER = "X-Access-Token";
     public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+
+    public static final String ALURA = "Alura";
+    public static final String ALURA_PIC = "Alura Pic";
+
+    public static final String AUTHORITIES = "authorities";
     public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
     public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
     public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
