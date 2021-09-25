d1c0a6608bdfbfb3da9ff690d3692f412b1838bf
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-25 05:47:16 -0300

    project init. Closes #262.

---
+++ alurapic/src/main/java/br/com/alura/alurapic/AlurapicApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.alurapic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlurapicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlurapicApplication.class, args);
+	}
+
+}
