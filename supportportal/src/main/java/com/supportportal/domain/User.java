64743d43721d82976c76b0320569eaab49ce7ae3
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-25 10:19:03 -0300

    Hide sensitive information. Closes #242.

--- supportportal/src/main/java/com/supportportal/domain/User.java
+++ supportportal/src/main/java/com/supportportal/domain/User.java
@@ -1,6 +1,7 @@
 package com.supportportal.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -18,11 +19,13 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
+    @JsonIgnore
     private Long id;
     private String userId;
     private String firstName;
     private String lastName;
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Transient
