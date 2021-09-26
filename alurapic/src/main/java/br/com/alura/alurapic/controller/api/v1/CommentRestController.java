e57e29454bfc287cbd2b35925184e3ae9bb320e2
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-26 18:10:19 -0300

    create CommentsController. Closes #282

---
+++ alurapic/src/main/java/br/com/alura/alurapic/controller/api/v1/CommentRestController.java
@@ -0,0 +1,48 @@
+package br.com.alura.alurapic.controller.api.v1;
+
+import br.com.alura.alurapic.domain.Comment;
+import br.com.alura.alurapic.domain.Photo;
+import br.com.alura.alurapic.exception.domain.CommentNotFoundException;
+import br.com.alura.alurapic.exception.domain.PhotoNotFounException;
+import br.com.alura.alurapic.exception.domain.UserNotFoundException;
+import br.com.alura.alurapic.service.photos.CommentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RequestMapping("/api/v1/photo/comment")
+public class CommentRestController {
+
+    private final CommentService commentService;
+
+    public CommentRestController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @ResponseStatus(OK)
+    @GetMapping("/{idPhoto}")
+    public List<Comment> getAllComments(@PathVariable String idPhoto) {
+        return commentService.getComments(Integer.parseInt(idPhoto));
+    }
+
+    @ResponseStatus(OK)
+    @PostMapping("/{idPhoto}")
+    public Photo addComment(@RequestParam("username") String username,
+                            @PathVariable String idPhoto,
+                            @RequestParam("comment") String comment)
+            throws UserNotFoundException, PhotoNotFounException {
+        return commentService.addComment(username, Integer.parseInt(idPhoto), comment);
+    }
+
+    @ResponseStatus(OK)
+    @DeleteMapping("/{idPhoto}/{idComment}")
+    public Photo deleteComment(@RequestParam("username") String username,
+                               @PathVariable String idPhoto,
+                               @PathVariable String idComment)
+            throws PhotoNotFounException, CommentNotFoundException {
+        return commentService.deleteComment(username,
+                Integer.parseInt(idPhoto), Integer.parseInt(idComment));
+    }
+}
