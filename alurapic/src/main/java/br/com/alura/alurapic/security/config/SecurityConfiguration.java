d51daa506c526eb0336dd74b34a3f3818b11d12e
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-29 11:58:55 -0300

    fix cors config to permit token.

--- alurapic/src/main/java/br/com/alura/alurapic/security/config/SecurityConfiguration.java
+++ alurapic/src/main/java/br/com/alura/alurapic/security/config/SecurityConfiguration.java
@@ -86,9 +86,9 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-                "Accept", "JWT-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Accept", "X-Access-Token", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "X-Access-Token", "Authorization",
                 "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
