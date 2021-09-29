5d639faa49e63c5a983f7254b5b7e018404f54c3
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-09-29 07:42:46 -0300

    Custom Message Component, for validators. Closes #302

--- alurapic/alurapic/src/app/home/home.module.ts
+++ alurapic/alurapic/src/app/home/home.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
+
 
 
 
@@ -11,7 +13,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule, 
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    VmessageModule
   ]
 })
 export class HomeModule { }
