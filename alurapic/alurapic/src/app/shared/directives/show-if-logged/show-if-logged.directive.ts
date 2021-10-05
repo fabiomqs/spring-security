c761f3153007e7a3132df50ad32ed2c63a2a7434
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-10-05 06:04:04 -0300

    Create Menu. Closes #343.

--- alurapic/alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ alurapic/alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -1,10 +1,15 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { UserService } from 'src/app/core/user/user.service';
+import { SubSink } from 'subsink';
 
 @Directive({
   selector: '[showIfLogged]'
 })
-export class ShowIfLoggedDirective implements OnInit{
+export class ShowIfLoggedDirective implements OnInit, OnDestroy {
+
+    private subs = new SubSink();
+
+    currentDisplay: string;
 
     constructor(
         private element: ElementRef<any>,
@@ -12,9 +17,27 @@ export class ShowIfLoggedDirective implements OnInit{
         private userService: UserService
     ) { }
     
+    
     ngOnInit(): void {
-        !this.userService.isLogged() &&
-            this.renderer.setStyle(this.element.nativeElement, 'display', 'none')
+        this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+
+        this.subs.add(
+            this.userService.getUser().subscribe(
+                user => {
+                    if(user) {
+                        this.renderer.setStyle(this.element.nativeElement, 
+                            'display', this.currentDisplay);
+                    } else {
+                        this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+                        this.renderer.setStyle(this.element.nativeElement, 'display', 'none');            
+                    }
+                }
+            )
+        )
+    }
+
+    ngOnDestroy(): void {
+        this.subs.unsubscribe();
     }
 
 }
